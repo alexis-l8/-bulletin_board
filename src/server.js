0776d63d0a9447024719fe741cc2b47121434005
Alexis Lui <alexislui@hotmail.co.uk>
2017-04-23 02:20:09 +0100

    Handlebars up and running Inert and Vision Required Fix #2

--- src/server.js
+++ src/server.js
@@ -2,10 +2,24 @@
 // Simple node.js code style tips to improve code quality - https://gist.github.com/hueniverse/a06f6315ea736ed1b46d
 const Hapi = require ('hapi');
 const server = new Hapi.Server;
+const routes = require('./routes')
+const inert = require('inert')
+const vision = require('vision')
+const handlebars = require('./handlebars')
 
 server.connection({
   port: process.env.PORT || 3000, //process.env.PORT sets the environment variable PORT to port your web server should listen to - http://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
   host:'localhost'
 })
+
+server.register([inert, vision], (err) => {
+  if (err) throw err;
+
+  server.views(handlebars);
+  server.route(routes);
+})
+
+
+
 
 module.exports = server;
